ad7c55fe176f252453eea2c91217356dad00e5fe
anahi-luna <rebeluna263@gmail.com>
2025-09-29 10:29:18 -0300

    Creación del modelo Cliente y conección con la base de datos

---
+++ Tp-AppVet/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tp_AppVet.Models;
+
+namespace Tp_AppVet.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ Tp-AppVet/Models/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tp_AppVet.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre no puede estar vacío")]
+        public required string Nombre { get; set; }
+        
+        [Required(ErrorMessage = "El mail no puede estar vacío")]
+        public required string Email { get; set; }
+        
+        [Required(ErrorMessage = "El telefono no puede estar vacío")]
+        public required string Telefono { get; set; }
+        
+
+    }
+}
